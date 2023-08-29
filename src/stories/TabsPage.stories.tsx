978546b7e272ed12c4db77cc4400301a2686557d
Adam Shehab <adamchehab@gmail.com>
2023-08-30 00:30:22 +0300

    Added: styling css (good), open and remove tab - W commit

---
+++ src/stories/TabsPage.stories.tsx
@@ -0,0 +1,9 @@
+import TabsPage from "../tabs/components/TabsPage/TabsPage";
+import "../css/index.css";
+
+export default {
+	title: "TabsPage",
+	component: TabsPage,
+};
+
+export const Default = () => <TabsPage />;
--- src/tabs/components/TabsPage/TabsPage.tsx
+++ src/tabs/components/TabsPage/TabsPage.tsx
@@ -1,61 +1,88 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { VscChromeClose } from "react-icons/vsc";
 import "./TabsPage.css";
 
 function Popup() {
-	const [tabs] = useState([
-		{
-			id: 1,
-			title: "Google",
-			url: "https://www.google.com",
-			favIconUrl: "https://www.google.com/favicon.ico",
-		},
-		{
-			id: 2,
-			title: "GitHub",
-			url: "https://github.com",
-			favIconUrl: "https://github.com/favicon.ico",
-		},
-		{
-			id: 3,
-			title: "Stack Overflow",
-			url: "https://stackoverflow.com",
-			favIconUrl:
-				"https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196",
-		},
-	]);
+	// Testing
+	// const [tabs] = useState([
+	// 	{
+	// 		id: 1,
+	// 		title: "Google",
+	// 		url: "https://www.google.com",
+	// 		favIconUrl: "https://www.google.com/favicon.ico",
+	// 	},
+	// 	{
+	// 		id: 2,
+	// 		title: "GitHub",
+	// 		url: "https://github.com",
+	// 		favIconUrl: "https://github.com/favicon.ico",
+	// 	},
+	// 	{
+	// 		id: 3,
+	// 		title: "Stack Overflow",
+	// 		url: "https://stackoverflow.com",
+	// 		favIconUrl:
+	// 			"https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196",
+	// 	},
+	// ]);
 
 	// NOTE http://localhost:5173/src/tabs/tabs.html - page
 
-	// const [tabs, setTabs] = useState([]);
+	const [tabs, setTabs] = useState([]);
 
-	// useEffect(() => {
-	// 	// Get tabs from storage
-	// 	chrome.storage.local.get(["myData"], (result) => {
-	// 		setTabs(result.myData);
-	// 	});
-	// }, []);
+	useEffect(() => {
+		// Get tabs from storage
+		chrome.storage.local.get(["myData"], (result) => {
+			setTabs(result.myData);
+		});
+	}, []);
 
 	const handleFaviconError = (e) => {
+		// FIX - still some icons are bad
 		e.target.src = "../../images/default_page.png";
+	};
+
+	const handleRemoveTab = (id) => {
+		chrome.storage.local.get(["myData"], (result) => {
+			const newTabs = result.myData.filter((item) => item.id !== id);
+			chrome.storage.local.set({ myData: newTabs }, () => {
+				setTabs(newTabs);
+			});
+		});
+	};
+
+	const handleCardClick = (item) => {
+		// TODO mabye make this an option
+		chrome.tabs.create({ url: item.url, active: false });
+		handleRemoveTab(item.id);
 	};
 
 	return (
 		<>
 			{tabs.map((item, index) => (
 				<div
-					className="flex border-2 border-[#212020] rounded-lg bg-[#303030] p-2 pl-2 m-1 shadow-md max-w-[265px] max-h-[51px] overflow-hidden items-center"
+					className="tab_card"
 					key={index}
+					onClick={() => handleCardClick(item)}
 				>
 					<img
-						className="w-8 h-8 mr-2"
+						className="icon"
 						src={item.favIconUrl}
 						onError={handleFaviconError}
 					/>
-					<a href={item.url} target="_blank">
+					<p>
+						{/* TODO fix length thing? */}
 						{item.title.length > 21
 							? `${item.title.slice(0, 21)}...`
 							: item.title}
-					</a>
+					</p>
+					<VscChromeClose
+						className="close"
+						onClick={(e) => {
+							e.stopPropagation();
+							handleRemoveTab(item.id);
+						}}
+					/>
 				</div>
 			))}
 		</>
