0e1bd2e633639754fb1dce048d7e99ba2c2ae2e4
Adam Shehab <adamchehab@gmail.com>
2023-08-30 12:58:50 +0300

    Fixed names

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PopupComponent from './components/PopupPage/PopupPage.tsx'
+import PopupPage from './components/PopupPage/PopupPage.tsx'
 import '../css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PopupComponent />
+    <PopupPage />
   </React.StrictMode>,
 )
--- src/tabs/tabs.tsx
+++ src/tabs/tabs.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PopupComponent from './components/TabsPage/TabsPage.tsx'
+import TabsPage from './components/TabsPage/TabsPage.tsx'
 import '../css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PopupComponent />
+    <TabsPage />
   </React.StrictMode>,
 )
